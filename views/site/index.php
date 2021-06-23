8dae7a8576d50c611fcb65b154b25a000cab7bee
Hancooler <187006072@student.unsil.ac.id>
2021-06-23 23:57:19 +0700

    css dan index

--- views/site/index.php
+++ views/site/index.php
@@ -5,49 +5,83 @@
 $this->title = 'My Yii Application';
 ?>
 <div class="site-index">
+ <!-- Header -->
 
-    <div class="jumbotron">
-        <h1>Congratulations!</h1>
+    <div class="intro-header">
+        <div class="container">
 
-        <p class="lead">You have successfully created your Yii-powered application.</p>
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="intro-message">
+                        <h1><strong>Selamat Datang</strong></h1>
+                        <h3> - GamePool Center - </h3>
+                    </div>
+                </div>
+            </div>
 
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
+        </div>
+        <!-- /.container -->
+
     </div>
+    <!-- /.intro-header -->
 
-    <div class="body-content">
+    <!-- Page Content -->
+    <!-- Page Content -->
 
-        <div class="row">
-            <div class="col-lg-4">
-                <h2>Heading</h2>
+	
+    <div class="content-section-a">
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-5 col-sm-6">
+                    <hr class="section-heading-spacer">
+                    <div class="clearfix"></div>
+                    <h2 class="section-heading"><strong>Mini Game For you</strong></h2>
+                    <p class="lead">Mari di coba <a target="_blank" href=""></a>mini game ini bisa dimainkan game yang tersedia merupakan game sederhana yang masih dalam tahap pengembangan.</p> 
+                </div>
+                <div class="col-lg-5 col-lg-offset-2 col-sm-6">
+                    <img class="img-responsive" src="https://image.freepik.com/free-vector/set-virtual-reality-equipment-elements_23-2148812130.jpg" alt="" width="370">
+                </div>
             </div>
         </div>
-
+        <!-- /.container -->
     </div>
+    <!-- /.content-section-a -->
+
+    <div class="content-section-b">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-5 col-lg-offset-1 col-sm-push-6  col-sm-6">
+                    <hr class="section-heading-spacer">
+                    <div class="clearfix"></div>
+                    <h2 class="section-heading"><strong>NEWS by Tim Creator</strong></h2>
+                    <p class="lead">Berita ini berisi konten tentang seputar teknologi dan game informasi yang ditampilkan disajikan dalam bentuk sebuah artikel atau buletin <a target="_blank" href="index.php?r=news%2Findex">News</a> Tidak ada unsur politik.</p>
+                </div>
+                <div class="col-lg-5 col-sm-pull-6  col-sm-6">
+                    <img class="img-responsive" src="https://image.freepik.com/free-vector/people-using-virtual-reality-glasses_23-2148770144.jpg" alt="" width="370">
+                </div>
+            </div>
+        </div>
+        <!-- /.container -->
+    </div>
+    <!-- /.content-section-b -->
+
+    <div class="content-section-a">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-5 col-sm-6">
+                    <hr class="section-heading-spacer">
+                    <div class="clearfix"></div>
+                    <h2 class="section-heading"><strong>Tentang Gamepool™</strong></h2>
+                    <p class="lead">Untuk mengenal lebih jauh silahkan klik <a target="_blank" href="index.php?r=site%2Fabout">link</a> ini.</p>
+                    <p class="lead">Ingin selalu up to date? Subscribe di <a target="_blank" href="https://rocky-scrubland-84244.herokuapp.com/">Newsletter</a> kami.</p>
+                </div>
+                <div class="col-lg-5 col-lg-offset-2 col-sm-6">
+                    <img class="img-responsive" src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/Infobox_info_icon.svg/1024px-Infobox_info_icon.svg.png" alt="" width=350>
+                </div>
+            </div>
+        </div>
+        <!-- /.container -->
+    </div>
+    <!-- /.content-section-a -->  
 </div>
